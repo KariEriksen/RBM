0bfd2aee2acfa6816c0a93c2bcf0b6f285f7a731
KariEriksen <eriksen_kari@hotmail.com>
2019-12-06 20:53:56 +0100

    arrays for all variational parameters

--- src/sampler.py
+++ src/sampler.py
@@ -12,22 +12,22 @@ class Sampler:
 
         self.local_energy = 0.0
         self.accumulate_energy = 0.0
-        self.accumulate_psi_term_a = np.array()
-        self.accumulate_psi_term_b = np.array()
-        self.accumulate_psi_term_W = np.array()
-        self.accumulate_both_a = np.array()
-        self.accumulate_both_b = np.array()
-        self.accumulate_both_W = np.array()
+        self.accumulate_psi_term_a = np.zeros(self.w.M)
+        self.accumulate_psi_term_b = np.zeros(self.w.N)
+        self.accumulate_psi_term_W = np.zeros((self.w.M, self.w.N))
+        self.accumulate_both_a = np.zeros(self.w.M)
+        self.accumulate_both_b = np.zeros(self.w.N)
+        self.accumulate_both_W = np.zeros((self.w.M, self.w.N))
         self.expec_val_energy = 0.0
-        self.expec_val_psi_a = np.array()
-        self.expec_val_psi_b = np.array()
-        self.expec_val_psi_W = np.array()
-        self.expec_val_both_a = np.array()
-        self.expec_val_both_b = np.array()
-        self.expec_val_both_W = np.array()
-        self.derivative_energy_a = np.array()
-        self.derivative_energy_b = np.array()
-        self.derivative_energy_W = np.array()
+        self.expec_val_psi_a = np.zeros(self.w.M)
+        self.expec_val_psi_b = np.zeros(self.w.N)
+        self.expec_val_psi_W = np.zeros((self.w.M, self.w.N))
+        self.expec_val_both_a = np.zeros(self.w.M)
+        self.expec_val_both_b = np.zeros(self.w.N)
+        self.expec_val_both_W = np.zeros((self.w.M, self.w.N))
+        self.derivative_energy_a = np.zeros(self.w.M)
+        self.derivative_energy_b = np.zeros(self.w.N)
+        self.derivative_energy_W = np.zeros((self.w.M, self.w.N))
 
     def sample_values(self, positions):
         """Get the local energy from Hamiltonian class"""
@@ -35,6 +35,10 @@ class Sampler:
 
         self.local_energy = self.h.local_energy(positions)
         self.accumulate_energy += self.h.local_energy(positions)
+        gradient_wf_a = np.zeros(self.w.M)
+        gradient_wf_b = np.zeros(self.w.N)
+        gradient_wf_W = np.zeros((self.w.M, self.w.N))
+
         gradient_wf_a = self.w.gradient_wavefunction_a(positions)
         gradient_wf_b = self.w.gradient_wavefunction_b(positions)
         gradient_wf_W = self.w.gradient_wavefunction_W(positions)
@@ -42,7 +46,9 @@ class Sampler:
         self.accumulate_psi_term_a += gradient_wf_a
         self.accumulate_psi_term_b += gradient_wf_b
         self.accumulate_psi_term_W += gradient_wf_W
-        self.accumulate_both += gradient_wf_a*self.local_energy
+        self.accumulate_both_a += gradient_wf_a*self.local_energy
+        self.accumulate_both_b += gradient_wf_b*self.local_energy
+        self.accumulate_both_W += gradient_wf_W*self.local_energy
 
     def average_values(self, monte_carlo_cycles):
         """Calculate average values"""
@@ -53,19 +59,28 @@ class Sampler:
         # for all variational parameters
         # calcualte the expectation values and the derivative of
         # the energy
-        for i in range(3):
-            self.expec_val_psi[i] = self.accumulate_psi_term[i]/mcc
-            self.expec_val_both[i] = self.accumulate_both[i]/mcc
-            derivative_energy_a = self.derivative_energy[i]
-            self.derivative_energy[i] = 2*(self.expec_val_both[i] -
-                                           (self.expec_val_psi[i] *
-                                            self.expec_val_energy))
+        self.expec_val_psi_a = self.accumulate_psi_term_a/mcc
+        self.expec_val_psi_b = self.accumulate_psi_term_b/mcc
+        self.expec_val_psi_W = self.accumulate_psi_term_W/mcc
+        self.expec_val_both_a = self.accumulate_both_a/mcc
+        self.expec_val_both_b = self.accumulate_both_b/mcc
+        self.expec_val_both_W = self.accumulate_both_W/mcc
+
+        self.derivative_energy_a = 2*(self.expec_val_both_a -
+                                      (self.expec_val_psi_a *
+                                       self.expec_val_energy))
+        self.derivative_energy_b = 2*(self.expec_val_both_b -
+                                      (self.expec_val_psi_b *
+                                       self.expec_val_energy))
+        self.derivative_energy_W = 2*(self.expec_val_both_W -
+                                      (self.expec_val_psi_W *
+                                       self.expec_val_energy))
 
     def print_avereges(self):
 
-        print ('deri energy param a = ', self.derivative_energy[0])
-        print ('deri energy param b = ', self.derivative_energy[1])
-        print ('deri energy param W = ', self.derivative_energy[2])
+        print ('deri energy param a = ', self.derivative_energy_a)
+        print ('deri energy param b = ', self.derivative_energy_b)
+        print ('deri energy param W = ', self.derivative_energy_W)
         print ('total energy =  ', self.local_energy)
         # energy/num_particles
         print ('----------------------------')
