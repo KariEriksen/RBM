6a339751c5b692e62a5229c85d64618860ef797c
KariEriksen <eriksen_kari@hotmail.com>
2019-12-10 18:47:51 +0100

    workes

--- test/test_hamiltonian.py
+++ test/test_hamiltonian.py
@@ -9,6 +9,7 @@ from Wavefunction.wavefunction import Wavefunction # noqa: 401
 
 
 def test_local_energy_2d_2p():
+    """test local energy without interaction"""
 
     num_particles = 2
     num_dimensions = 2
@@ -32,6 +33,35 @@ def test_local_energy_2d_2p():
             x += positions[i]*positions[i]
 
         energy = 0.5*(-fd*fd + sd + omega*omega*x)
-        assert 0 == 0
-        # assert energy == pytest.approx(hamilton.local_energy(positions),
-        #                               abs=1e-14)
+
+        assert energy == pytest.approx(hamilton.local_energy(positions),
+                                      abs=1e-14)
+
+
+def test_local_energy_3d_2p():
+
+    num_particles = 3
+    num_dimensions = 2
+    M = num_particles*num_dimensions
+    N = 3
+    a = np.random.uniform(-2, 2, M)
+    b = np.random.uniform(-2, 2, N)
+    W = np.random.uniform(-2, 2, (M, N))
+    omega = 1.0
+    gamma = 1.0
+    sigma = 1.0
+    wave = Wavefunction(M, N, a, b, W, sigma)
+    hamilton = Hamiltonian(gamma, omega, num_dimensions,
+                           num_particles, wave, 'false')
+
+    for _ in range(50):
+        positions = np.random.uniform(-2, 2, M)
+        fd, sd = wave.gradients_wavefunction(positions)
+        x = 0.0
+        for i in range(M):
+            x += positions[i]*positions[i]
+
+        energy = 0.5*(-fd*fd + sd + omega*omega*x)
+
+        assert energy == pytest.approx(hamilton.local_energy(positions),
+                                      abs=1e-14)
